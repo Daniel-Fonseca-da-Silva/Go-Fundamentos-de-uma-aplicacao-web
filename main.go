4b5f1b22ea6b8a7ce003f9f7cced9c81951f33c4
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-05 16:47:20 -0300

    Modularizado produtos

--- main.go
+++ main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"daniel/db"
+	"daniel/models"
 	"net/http"
 	"text/template"
 )
-
-type Produto struct {
-	Id         int
-	Nome       string
-	Descricao  string
-	Preco      float64
-	Quantidade int
-}
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
@@ -22,36 +14,8 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	db := db.ConectaComBancoDeDados()
+	todosOsProdutos := models.BuscaTodosOsProdutos()
 
-	selectDeTodosOsProdutos, err := db.Query("SELECT * FROM produtos")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p := Produto{}
-	produtos := []Produto{}
-
-	for selectDeTodosOsProdutos.Next() {
-		var id, quantidade int
-		var nome, descricao string
-		var preco float64
-
-		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		p.Nome = nome
-		p.Descricao = descricao
-		p.Preco = preco
-		p.Quantidade = quantidade
-
-		produtos = append(produtos, p)
-
-	}
-
-	temp.ExecuteTemplate(w, "Index", produtos)
-	defer db.Close()
+	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
 
 }
---
+++ models/produtos.go
@@ -0,0 +1,43 @@
+package models
+
+import "daniel/db"
+
+type Produto struct {
+	Id         int
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
+func BuscaTodosOsProdutos() []Produto {
+	db := db.ConectaComBancoDeDados()
+
+	selectDeTodosOsProdutos, err := db.Query("SELECT * FROM produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
+	}
+	db.Close()
+	return produtos
+}

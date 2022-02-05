78490c826722bd18533e8f531379bd741f3afe7b
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-05 01:49:56 -0300

    Criando struct de produtos

--- main.go
+++ main.go
@@ -5,6 +5,13 @@ import (
 	"text/template"
 )
 
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -13,6 +20,15 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	produtos := []Produto{
+		{Nome: "Relógio ourax", Descricao: "Super moderno e elegante", Preco: 13299.99, Quantidade: 15},
+		{"Teclado Jumpo", "Teclado por ondas radionicas", 59.99, 851},
+		{"Video game grudano", "Super video game perca vida real", 89.99, 551},
+		{"Fonte potente nanomax", "Super fonte para nave espacial", 885.99, 125},
+		{"Espada alienigena da era antiga", "Espada encontrada no planeta zapio", 1359.99, 2},
+		{"Esqueleto de humanoides 2022", "Esqueleto humanoide usado para hologramar", 359.99, 155},
+		{"Pistola de dupla materia 2055", "Super pistola com disparo quadriplo ou triplo", 3285.74, 78},
+	}
+	temp.ExecuteTemplate(w, "Index", produtos)
 
 }

20a99a7aaebf8d39b8b7a651adb95b116606340b
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-06 01:08:06 -0300

    Código duplicado, página login, e partials adicionados

--- controllers/produtosController.go
+++ controllers/produtosController.go
@@ -11,9 +11,13 @@ import (
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "Index", nil)
+}
+
+func Product(w http.ResponseWriter, r *http.Request) {
 	todosOsProdutos := models.BuscaTodosOsProdutos()
 
-	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
+	temp.ExecuteTemplate(w, "Product", todosOsProdutos)
 }
 
 func New(w http.ResponseWriter, r *http.Request) {
--- routes/routes.go
+++ routes/routes.go
@@ -7,6 +7,7 @@ import (
 
 func CarregaRotas() {
 	http.HandleFunc("/", controllers.Index)
+	http.HandleFunc("/product", controllers.Product)
 	http.HandleFunc("/new", controllers.New)
 	http.HandleFunc("/insert", controllers.Insert)
 	http.HandleFunc("/delete", controllers.Delete)
